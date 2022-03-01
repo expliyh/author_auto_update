f99d867bb820336edf4ce2f6f6835cc3fa17bc68
expliyh <expliyh@outlook.com>
2022-03-01 22:22:24 +0800

    Changes

---
+++ database.py
@@ -0,0 +1,46 @@
+import pymysql
+
+
+class Database:
+    def __init__(self, basename, username, address, password):
+        self.db = pymysql.connect(host=address, user=username, password=password, database=basename)
+        return
+
+    def max_id(self):
+        sql = "SELECT MAX(id) FROM `imginfo` WHERE 1"
+        cursor = self.db.cursor()
+        try:
+            cursor.execute(sql)
+            results = cursor.fetchall()
+            # print(results)
+            for row in results:
+                idd = row[0]
+            return idd
+        except pymysql.Error:
+            return 'Error'
+
+    def get_image_id(self, idd):
+        sql = "SELECT * FROM `imginfo` WHERE `id` = '%s'" % idd
+        cursor = self.db.cursor()
+        try:
+            cursor.execute(sql)
+            results = cursor.fetchall()
+            img_id = -1
+            for row in results:
+                img_id = row[5]
+            return img_id
+        except pymysql.Error:
+            return 'Error'
+
+    def update_author(self, idd, author: str, author_id):
+        cursor = self.db.cursor()
+        sql = "UPDATE `imginfo` SET `author`='%s',`author_id`='%s' WHERE `id`='%s'" % (author, author_id, idd)
+        try:
+            # 执行sql语句
+            cursor.execute(sql)
+            # 提交到数据库执行
+            self.db.commit()
+        except pymysql.Error:
+            # 如果发生错误则回滚
+            self.db.rollback()
+        cursor.close()
