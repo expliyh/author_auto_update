9bf73bec99ab111e5ab074bfe157fd3afa65fabe
expliyh <54161210+expliyh@users.noreply.github.com>
2022-05-10 22:26:45 +0800

    Update database.py

--- database.py
+++ database.py
@@ -19,7 +19,7 @@ class Database:
             # print"db is connecting"
         except:
             # traceback.print_exc()
-            self.db = self.to_connect()
+            self.db = self.connect()
             print("db reconnect")
 
     def max_id(self):
@@ -64,10 +64,9 @@ class Database:
             self.db.rollback()
         cursor.close()
 
-    def update_all(self, idd, title: str, author: str, pixiv_id, author_id, tags: str, caption: str,
-                   original_url: str, reply: str):
+    def update_all(self, idd, title: str, author: str, pixiv_id, author_id, tags: str, caption: str, original_url: str, reply: str):
         cursor = self.db.cursor()
-        sql = "UPDATE `imginfo` SET `name`='%s',`author`='%s',`pixivid`='%s',`author_id`='%s',`tags`='%s',`caption`='%s',`original_url`='%s',`reply`='%s' WHERE `id`='%s'" % (title, author, pixiv_id, author_id, tags, caption, original_url, reply, idd )
+        sql = "UPDATE `imginfo` SET `name`='%s',`author`='%s',`pixivid`='%s',`author_id`='%s',`tags`='%s',`caption`='%s',`original_url`='%s',`reply`='%s' WHERE `id`='%s'" % (title, author, pixiv_id, author_id, tags, caption, original_url, reply, idd)
         self.is_connected()
         try:
             # 执行sql语句
