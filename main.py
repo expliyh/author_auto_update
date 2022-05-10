1a83a454c9e2d93415c194ec215b3317d5ec9bd3
expliyh <54161210+expliyh@users.noreply.github.com>
2022-05-10 23:33:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,49 @@ print("Inits")
 
 # 按间距中的绿色按钮以运行脚本。
 
+
+def update(databasel, iddd):
+    pixiv_idl = databasel.get_image_id(iddd)
+    print('\n')
+    params = {'id': pixiv_idl}
+    information = requests.get(url='https://api.moedog.org/pixiv/v2/', params=params)
+    json_info = json.loads(information.text)
+    print(json_info)
+    print('\n')
+    img_name = json_info['illust']['title']
+    author_name = json_info['illust']['user']['name']
+    author_id = json_info['illust']['user']['id']
+    tags = json_info['illust']['tags']
+    caption = json_info['illust']['caption']
+    try:
+        original_url = json_info['meta_single_page']['original_image_url']
+    except:
+        original_url = 'Empty'
+
+    tagstxt = json.dumps(tags, ensure_ascii=False)
+    tags_converted = ''
+    for r in tagstxt:
+        if r == '\'':
+            tags_converted += '\\'
+        tags_converted += r
+    cap_converted = ''
+    for r in caption:
+        if r == '\'':
+            cap_converted += '\\'
+        cap_converted += r
+    rep_converted = ''
+    for r in information.text:
+        if r == '\'':
+            rep_converted += '\\'
+        rep_converted += r
+    print("Updating image %s name:%s author:%s author_id:%s" % (i, img_name, author_name, author_id))
+    print("Tags: %s" % tagstxt)
+    print("Captions: %s" % caption)
+    databasel.update_all(iddd, img_name, author_name, pixiv_idl, author_id, tags_converted, cap_converted, original_url,
+                         rep_converted)
+    # print(information.text)
+
+
 if __name__ == '__main__':
     # database_name = os.environ['database']
     # username = os.environ['username']
@@ -39,45 +82,7 @@ if __name__ == '__main__':
     print('There are %s images in the database, updating.\n' % (idd + 1))
 
     for i in range(idd + 1):
-        pixiv_idl = db.get_image_id(i)
-        print('\n')
-        params = {'id': pixiv_idl}
-        information = requests.get(url='https://api.moedog.org/pixiv/v2/', params=params)
-        json_info = json.loads(information.text)
-        print(json_info)
-        print('\n')
-        img_name = json_info['illust']['title']
-        author_name = json_info['illust']['user']['name']
-        author_id = json_info['illust']['user']['id']
-        tags = json_info['illust']['tags']
-        caption = json_info['illust']['caption']
-        try:
-            original_url = json_info['meta_single_page']['original_image_url']
-        except:
-            original_url = 'Empty'
-
-        tagstxt = json.dumps(tags, ensure_ascii=False)
-        tags_converted = ''
-        for r in tagstxt:
-            if r == '\'':
-                tags_converted += '\\'
-            tags_converted += r
-        cap_converted = ''
-        for r in caption:
-            if r == '\'':
-                cap_converted += '\\'
-            cap_converted += r
-        rep_converted = ''
-        for r in information.text:
-            if r == '\'':
-                rep_converted += '\\'
-            rep_converted += r
-        print("Updating image %s name:%s author:%s author_id:%s" % (i, img_name, author_name, author_id))
-        print("Tags: %s" % tagstxt)
-        print("Captions: %s" % caption)
-        db.update_all(i, img_name, author_name, pixiv_idl, author_id, tags_converted, cap_converted,
-                      original_url, rep_converted)
-        # print(information.text)
+        update(db, i)
         # pixiv_id = db.get_image_id(i)
         # illustration = pixiv_client.fetch_illustration(pixiv_id)
         # author = illustration.user
